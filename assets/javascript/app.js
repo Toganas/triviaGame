85de2d7f146cbdef2358e6328ffc7cd52835ee89
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-06-13 21:06:25 -0500

    timer added, no floor though

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+
+// run this ever time a new question is asked
+function newQuestion() {
+
+    // countdown timer
+    var timer = 30;
+    $("#timer").append(timer);
+    var seconds = setInterval(countdown, 1000);
+
+    function countdown() {
+        timer--
+        $("#timer").text(timer);
+    }
+}
+
+newQuestion();
+
